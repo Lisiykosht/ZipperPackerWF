5be65e5c4193500c0de1c40ec5bde98551b47c11
Kosta Zloff <htzr@live.ru>
2021-04-04 00:24:14 +0300

    Добавьте файлы проекта.

---
+++ Dict.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Collections.Specialized;
+
+
+namespace ZipperPackerWF
+{
+    
+    public partial class Dict : Form
+    {
+        public NameValueCollection sAll;
+        public Dict()
+        {
+            InitializeComponent();
+        }
+
+        private void Dict_Load(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            label1.Text = "Код ЛПУ";
+            label2.Text = "Пароль";
+           
+            button1.Text = "Сохранить";
+
+            sAll = ConfigurationManager.AppSettings;
+
+            foreach (string s in sAll.AllKeys)
+            { if (s.StartsWith("15")){ textBox1.AppendText(s + " " + sAll.Get(s)+"\r\n"); } }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[,] strarr = new string[ textBox1.Lines.Length,2];
+            int roww = 0;
+            foreach (string s in textBox1.Lines)
+            {
+                string[] words = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                   
+                    if (words[0].StartsWith("15") && words[0].Length == 6)
+                    {
+                        strarr[roww, 0] = words[0];
+                        strarr[roww, 1] = words[1];
+                        roww += 1;
+                    }
+                }
+                catch
+                {
+
+                }
+             
+
+            }
+            
+                for (int i=0;i<roww;i++)
+                {
+               
+                    if (!ConfigurationManager.AppSettings.AllKeys.Contains(strarr[i,0]))
+                    {
+                        // открываем текущий конфиг специальным обьектом
+                        System.Configuration.Configuration currentConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                        // добавляем позицию в раздел AppSettings
+                        currentConfig.AppSettings.Settings.Add(strarr[i, 0], strarr[i, 1]);
+                        //сохраняем
+                        currentConfig.Save(ConfigurationSaveMode.Full);
+                        //принудительно перезагружаем соотвествующую секцию
+                        ConfigurationManager.RefreshSection("appSettings");
+                }
+                else
+                {
+                    System.Configuration.Configuration currentConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                    currentConfig.AppSettings.Settings[strarr[i,0]].Value = strarr[i,1];
+
+                    currentConfig.Save(ConfigurationSaveMode.Modified);
+                    ConfigurationManager.RefreshSection("appSettings");
+                }
+                } 
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ionic.Zip;
+using System.Threading;
+using System.Configuration;
+using System.Collections.Specialized;
+
+
+namespace ZipperPackerWF
+{
+
+    public partial class Form1 : Form
+    {
+        private NameValueCollection sAll;
+        public int time_1 = 1;
+        public Form1()
+        {
+            InitializeComponent();
+
+            sAll = ConfigurationManager.AppSettings;
+            if (sAll.Get("delete_orig") == "1")
+            { checkBox1.Checked = true; }
+            else { checkBox1.Checked = false; };
+            checkBox1.Text = "Удалять исходные файлы";
+            button1.Text = "Пароли";
+
+            label2.Text = time_1.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (time_1 > 0)
+            { time_1 -= 1; }
+            else
+            {
+                Doer();
+                time_1 = 10;
+            }
+
+            label2.Text = time_1.ToString();
+        }
+
+
+
+        private void Doer()
+        {
+            ConfigurationManager.RefreshSection("appSettings");
+            DirectoryInfo di_pack = new DirectoryInfo(@".\Incoming");
+            DirectoryInfo di_packed = new DirectoryInfo(@".\Outgoing\");
+            if (!di_pack.Exists)
+            { di_pack.Create(); }
+
+            if (!di_packed.Exists)
+            { di_packed.Create(); }
+
+
+            foreach (FileInfo findedFile in di_pack.GetFiles())
+            {
+                listBox1.Items.Add(DateTime.Now + "  ||    Пакуем :" + findedFile.Name);
+                //  listBox1.Items.Add();
+                string pwd = sAll.Get(findedFile.Name.Substring(0, 6));
+                if (pwd!=null)
+                {
+                    using (ZipFile zip = new ZipFile())
+                    {
+                        try
+                        {
+                            zip.Password = pwd;
+                            zip.AddFile(di_pack + @"\"+findedFile.Name).FileName = findedFile.Name;
+                           
+                            zip.Save(di_packed + @"\" + Path.GetFileNameWithoutExtension(findedFile.Name) + ".zip");
+                           
+
+                            if (checkBox1.Checked) { findedFile.Delete(); }
+
+                            listBox1.Items.Add(DateTime.Now + "  ||    Файл " + findedFile.Name + " упакован c паролем "+pwd+" !");
+                        }
+                       catch
+                        {
+                            
+                            listBox1.Items.Add("Сбой! Секундочку...");
+                            Thread.Sleep(1000);
+                        }
+                    }
+
+
+                }
+                else 
+                {
+                    listBox1.Items.Add("Сбой! Пароль не задан или неправилльное имя файла!");
+                }
+
+
+
+
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dict dictForm = new Dict();
+            
+            dictForm.Show();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            string attr;
+            if (checkBox1.Checked) { attr = "1"; } else { attr = "0"; };
+
+            System.Configuration.Configuration currentConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            currentConfig.AppSettings.Settings["delete_orig"].Value = attr;
+
+            currentConfig.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        
+    }
+    }
+}
